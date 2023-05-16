ed42562390abf4a03ac21d1b2400ec8290b27d10
Liam190100 <ngugi1652@gmail.com>
2023-05-16 03:21:46 -0700

    betty.c

---
+++ betty.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry of the program
+ *
+ * Return: Always 0 (on successful execution)
+ */
+int main(void)
+{
+	char name[] = "ALX student";
+
+	printf("Name: %s\n", name);
+
+	return (0);
+}
+
