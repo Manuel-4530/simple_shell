1cff3a39b00be6d17cc02f574059efd6f4f7318d
emmanuel <kivindyoemanuel@gmail.com>
2023-05-19 04:49:19 -0700

    updated files

--- skell.c
+++ skell.c
@@ -1,55 +1,100 @@
 #include "shell.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+void skell(char **env);
+char *read_input(void);
+void process_input(char *input, char **env);
+void execute_command(char *command, char **env);
+
+/**
+ * skell - Function to execute commands in the shell.
+ * @env: The environment variable.
+ */
 void skell(char **env)
 {
-    char *string = NULL;
-    int x, status;
-    size_t n = 0;
-    ssize_t num_char;
-    char *args[] = {NULL, NULL};
-    pid_t kid_pid;
-
-    while (1)
-    {
-        printf("cisfun mzee$ ");
-
-        num_char = getline(&string, &n, stdin);
-        if (num_char == -1)
-        {
-            free(string);
-            exit(EXIT_FAILURE);
-        }
-
-        x = 0;
-        while (string[x])
-        {
-            if (string[x] == '\n')
-                string[x] = '\0';
-            x++;
-        }
-
-        args[0] = string;
-        kid_pid = fork();
-		 if (kid_pid == -1)
-        {
-            free(string);
-            exit(EXIT_FAILURE);
-        }
-        else if (kid_pid == 0)
-        {
-            if (execve(args[0], args, env) == -1)
-            {
-                printf("%s: No such file or directory\n", args[0]);
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            wait(&status);
-        }
-    }
+	char *input;
+	while (1)
+	{
+		printf("cisfun mzee$ ");
+		fflush(stdout);
+		input = read_input();
+		process_input(input, env);
+		free(input);
+	}
 }
+
+/**
+ * read_input - Read input from the user.
+ * Return: The input string.
+ */
+char *read_input(void)
+{
+	char *input = NULL;
+	size_t n = 0;
+	ssize_t num_char = getline(&input, &n, stdin);
+
+	if (num_char == -1)
+	{
+		free(input);
+		exit(EXIT_FAILURE);
+	}
+
+	if (input[num_char - 1] == '\n')
+		input[num_char - 1] = '\0';
+
+	return input;
+}
+
+/**
+ * process_input - Process the user input.
+ * @input: The user input string.
+ * @env: The environment variable.
+ */
+void process_input(char *input, char **env)
+{
+	char *command = strtok(input, " \t\n");
+
+	while (command != NULL)
+	{
+		execute_command(command, env);
+		command = strtok(NULL, " \t\n");
+	}
+}
+
+/**
+ * execute_command - Execute a command.
+ * @command: The command string.
+ * @env: The environment variable.
+ */
+void execute_command(char *command, char **env)
+{
+	pid_t pid = fork();
+
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		char *args[2];
+		memset(args, 0, sizeof(args));
+		strcpy(args[0], command);
+
+		if (execve(args[0], args, env) == -1)
+		{
+			printf("%s: No such file or directory\n", args[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		int status;
+		wait(&status);
+	}
+}
+
--- skellin.c
+++ skellin.c
@@ -1,8 +1,18 @@
 #include "shell.h"
 
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command line arguments.
+ * @env: The environment variable.
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char **env)
 {
-	if(argc == 1)
-		skell(env);
-	return 0;
+	if (argc == 1)
+	{
+		skell(env); /* Call the skell function if no arguments are provided */
+	}
+	return (0);
 }
+
