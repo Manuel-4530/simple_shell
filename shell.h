93f1797a6fba53086a04f39611e3377e43d9c726
emmanuel <kivindyoemanuel@gmail.com>
2023-05-23 06:37:47 -0700

    shell proto

--- shell.h
+++ shell.h
@@ -4,17 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait>
+#include <sys/wait.h>
 
-void _skell(char *argv, char **env);
-int main(int argc, char **argv **env)
-=======
 void skell(char **env);
 char *read_input(void);
 void process_input(char *input, char **env);
 void execute_command(char *command, char **env);
 void exit_shell(void);
 void env_shell(char **env);
+void execute_command_with_args(char *command, char **args, char **env);
+void process_input_with_args(char *input, char **env);
+void handle_command_with_args(char **args, char **env);
+int handle_main(void);
 
 #endif
 
