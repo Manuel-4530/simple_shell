8b6718bd17a78900cb9215e104ee94237eb79fb8
emmanuel <kivindyoemanuel@gmail.com>
2023-05-25 03:41:37 -0700

    and more files

--- cmd_arg.c
+++ cmd_arg.c
@@ -7,34 +7,34 @@
 #include <unistd.h>
 
 /**
- * execute_command_with_args - Execute a command with arguments.
- * @command: The command string.
- * @args: Array of arguments.
- * @env: The environment variable.
- */
+* execute_command_with_args - Execute a command with arguments.
+* @command: The command string.
+* @args: Array of arguments.
+* @env: The environment variable.
+*/
 void execute_command_with_args(char *command, char **args, char **env)
 {
-    pid_t pid = fork();
+pid_t pid = fork();
 
-    if (pid == -1)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        /* Child process */
-        if (execve(command, args, env) == -1)
-        {
-            perror("execve");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        /* Parent process */
-        int status;
-        waitpid(pid, &status, 0);
-    }
+if (pid == -1)
+{
+perror("fork");
+exit(EXIT_FAILURE);
+}
+else if (pid == 0)
+{
+/* Child process */
+if (execve(command, args, env) == -1)
+{
+perror("execve");
+exit(EXIT_FAILURE);
+}
+}
+else
+{
+/* Parent process */
+int status;
+waitpid(pid, &status, 0);
+}
 }
 
