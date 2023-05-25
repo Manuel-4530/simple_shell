cf8fab8b4832d5b210a7dfaa37b06d9fcb06f0c3
emmanuel <kivindyoemanuel@gmail.com>
2023-05-25 04:40:21 -0700

    shell file

--- shell.h
+++ shell.h
@@ -6,12 +6,21 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+/*global variables*/
 void skell(char **env);
 char *read_input(void);
+
+/* main function functions*/
 void process_input(char *input, char **env);
 void execute_command(char *command, char **env);
+
+/*exit builtin*/ 
 void exit_shell(void);
+
+/*envirnment */
 void env_shell(char **env);
+
+/*execute functions*/
 void execute_command_with_args(char *command, char **args, char **env);
 void process_input_with_args(char *input, char **env);
 void handle_command_with_args(char **args, char **env);
